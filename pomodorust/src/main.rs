6da106b81489698384f03bb01fb4036299c2c3f3
Hermit14n <Hermit14n@gmail.com>
2023-11-12 10:39:22 -0700

    documenting initial plan

--- pomodorust/src/main.rs
+++ pomodorust/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Want to accept args
+        // work time             --wt <f32>
+        // break time            --bt <f32>
+        // progress bar yes/no   --pb <bool>
+        // pretty view           --pv <bool>
+
+
 }
