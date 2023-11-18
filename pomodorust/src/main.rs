d753bd9e5619939df293850238a5a6594b1c7029
Hermit14n <Hermit14n@gmail.com>
2023-11-18 09:54:39 -0700

    small stuff
    adding separate thread for timer
    
    added some comments

--- pomodorust/src/main.rs
+++ pomodorust/src/main.rs
@@ -1,25 +1,55 @@
 use pomodorust::{BreakTimer, WorkTimer, Timer};
-
+use clap::{arg, command};
+use std::sync::mpsc;
+use std::thread;
 
 
 fn main() {
-    let worktime = 8.0;
-    let breaktime = 10.0;
-    let timer = Timer::new_timer(worktime, breaktime);
-    let _worktimer = timer.start_work().unwrap();
+    let mut worktime = 8.0;
+    let mut breaktime = 10.0;
 
+    let (tx, _rx) = mpsc::channel();
+
+    let matches = command!()
+        .arg(arg!(
+            -w --worktime <f64>... "Add work length in minutes"
+        ).value_parser(clap::value_parser!(f64)))
+        .arg(arg!(
+            -b --breaktime <f64> ... "Add break length in minutes"
+        ).value_parser(clap::value_parser!(f64)))
+        .get_matches();
+    
+    if let Some(cli_worktime) = matches.get_one::<f64>("worktime") {
+        worktime = *cli_worktime * 60.0;
+    }
+
+    if let Some(cli_breaktime) = matches.get_one::<f64>("breaktime") {
+        breaktime = *cli_breaktime * 60.0;
+    }
+
+    
+    let timer = Timer::new_timer(worktime, breaktime);
+    let handle = thread::spawn(move || {
+        let _worktimer = timer.start_work().unwrap();
+        tx.send(_worktimer).expect("Problem with channel to worker thread");
+        
     loop {
         
         let _breaktimer: BreakTimer = _worktimer.start_break().unwrap();
         let _worktimer: WorkTimer = _breaktimer.start_work().unwrap();
-    }
-    
+
+         } 
+    });
+
+    handle.join().expect("Thread panicked"); // termination of the main thread will also
+                                                  // terminate child thread, join keeps
+                                                  // main in scope so child thread can run
 
     // Want to accept args
-        // work time             --wt <f64>
-        // break time            --bt <f64>
-        // progress bar yes/no   --pb <bool>
-        // pretty view           --pv <bool>
+        // work time             -w <f64>
+        // break time            -b <f64>
+        // progress bar yes/no   -p <bool>
+        // pretty view           -v <bool>
 }
 
 
